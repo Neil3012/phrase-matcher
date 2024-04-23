2d07396ccfd463aafdf3971992232237c69d7a22
Neil3012 <63674799+Neil3012@users.noreply.github.com>
2024-04-23 14:21:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+var SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
 
 var phrases = [
   'I love to sing because it\'s fun',
@@ -19,46 +19,16 @@ var diagnosticPara = document.querySelector('.output');
 
 var testBtn = document.querySelector('button');
 
-// Check if the device is running iOS 17
-const isIOS17 = () => {
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  return /iPad|iPhone|iPod/.test(userAgent) && /OS 17_/.test(userAgent);
-};
-
-// Function to request microphone access
-const requestMicrophonePermission = async () => {
-  try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      console.log('Microphone access granted!', stream);
-      // Now you can use the microphone stream for audio processing
-  } catch (error) {
-      console.error('Error requesting microphone:', error);
-      testBtn.textContent = 'Error requesting microphone:', error;
-
-
-  }
-};
-
-// Check if the device is iOS 17 and request microphone permission
-if (isIOS17()) {
-  console.log('Requesting microphone due to audio issue in iOS 17.');
-  // Perform microphone permission request when needed (e.g., on user interaction)
-  document.addEventListener('click', () => {
-      requestMicrophonePermission();
-  });
-} else {
-  console.log('Device is not running iOS 17. No need to request microphone.');
-}
 function randomPhrase() {
   var number = Math.floor(Math.random() * phrases.length);
-  return number;
+  return phrases[number];
 }
 
 function testSpeech() {
   testBtn.disabled = true;
   testBtn.textContent = 'Test in progress';
 
-  var phrase = phrases[randomPhrase()];
+  var phrase = randomPhrase();
   // To ensure case consistency while checking with the returned output text
   phrase = phrase.toLowerCase();
   phrasePara.textContent = phrase;
@@ -66,7 +36,7 @@ function testSpeech() {
   resultPara.style.background = 'rgba(0,0,0,0.2)';
   diagnosticPara.textContent = '...diagnostic messages';
 
-  var grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + phrase +';';
+  var grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + phrase + ';';
   var recognition = new SpeechRecognition();
   var speechRecognitionList = new SpeechGrammarList();
   speechRecognitionList.addFromString(grammar, 1);
@@ -78,17 +48,9 @@ function testSpeech() {
   recognition.start();
 
   recognition.onresult = function(event) {
-    // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-    // It has a getter so it can be accessed like an array
-    // The first [0] returns the SpeechRecognitionResult at position 0.
-    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-    // These also have getters so they can be accessed like arrays.
-    // The second [0] returns the SpeechRecognitionAlternative at position 0.
-    // We then return the transcript property of the SpeechRecognitionAlternative object 
     var speechResult = event.results[0][0].transcript.toLowerCase();
     diagnosticPara.textContent = 'Speech received: ' + speechResult + '.';
-    if(speechResult === phrase) {
+    if (speechResult === phrase) {
       resultPara.textContent = 'I heard the correct phrase!';
       resultPara.style.background = 'lime';
     } else {
@@ -97,59 +59,24 @@ function testSpeech() {
     }
 
     console.log('Confidence: ' + event.results[0][0].confidence);
-  }
+  };
 
   recognition.onspeechend = function() {
     recognition.stop();
     testBtn.disabled = false;
     testBtn.textContent = 'Start new test';
-  }
+  };
 
   recognition.onerror = function(event) {
     testBtn.disabled = false;
     testBtn.textContent = 'Start new test';
     diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
-  }
-  
-  recognition.onaudiostart = function(event) {
-      //Fired when the user agent has started to capture audio.
-      console.log('SpeechRecognition.onaudiostart');
-  }
-  
-  recognition.onaudioend = function(event) {
-      //Fired when the user agent has finished capturing audio.
-      console.log('SpeechRecognition.onaudioend');
-  }
-  
-  recognition.onend = function(event) {
-      //Fired when the speech recognition service has disconnected.
-      console.log('SpeechRecognition.onend');
-  }
-  
-  recognition.onnomatch = function(event) {
-      //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-      console.log('SpeechRecognition.onnomatch');
-  }
-  
-  recognition.onsoundstart = function(event) {
-      //Fired when any sound — recognisable speech or not — has been detected.
-      console.log('SpeechRecognition.onsoundstart');
-  }
-  
-  recognition.onsoundend = function(event) {
-      //Fired when any sound — recognisable speech or not — has stopped being detected.
-      console.log('SpeechRecognition.onsoundend');
-  }
-  
-  recognition.onspeechstart = function (event) {
-      //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-      console.log('SpeechRecognition.onspeechstart');
-  }
-  recognition.onstart = function(event) {
-      //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
-      console.log('SpeechRecognition.onstart');
-  }
+  };
 }
 
-
 testBtn.addEventListener('click', testSpeech);
+
+// For iOS, add an event listener to start speech recognition on button click
+if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+  testBtn.addEventListener('touchstart', testSpeech);
+}
